10a48844a0a3b4b610104b31f9b1cc8b01ecbb9f
baharriyan <indonesiariyan@gmail.com>
2020-10-20 18:48:52 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN2_Variable/src/PBOIF2_10116416_LATIHAN2/PBOIF210116416Latihan2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOIF2_10116416_LATIHAN2;
+
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class PBOIF210116416Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
